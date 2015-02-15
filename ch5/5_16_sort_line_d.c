8781d7fe6682cccbb7701872590df992c5e33a03
wangyu <nameiswangyu@163.com>
2015-02-15 15:24:16 +0800

    ch5

---
+++ ch5/5_16_sort_line_d.c
@@ -0,0 +1,8 @@
+/*
+ * 5-16. Add the -d (``directory order'') option, which makes comparisons only on
+ * letters, numbers and blanks. Make sure it works in conjunction with -f .
+ *
+ * wy-ei 2015.2.13
+*/
+
+
