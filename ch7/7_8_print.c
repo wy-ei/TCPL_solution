73764296dd9f359fad70be20c470e79e5fa885a5
wangyu <nameiswangyu@163.com>
2015-02-18 00:55:28 +0800

    ch7

--- ch7/7_8_print.c
+++ ch7/7_8_print.c
@@ -3,7 +3,7 @@
  * with a title and a running page count for each file.
  *
  * wy-ei 2015.2.16
-*/
+ */
 
 #include <stdio.h>
 #define MAXLEN 1000
@@ -24,6 +24,8 @@ int main(int argc,char *argv[]){
 			fprintf(stderr,"error:open file fail\n");
 			return 1;
 		}
+		page=0;
+		linenum=0;
 		while(fgets(line,MAXLEN,fp)!=NULL){
 			if(linenum%rows==0){
 				if(page>0){
@@ -44,6 +46,13 @@ int main(int argc,char *argv[]){
 		int i;
 		for(i=linenum%rows;i<rows;i++){
 			putchar('\n');
+		}
+
+		int c;
+		printf("type q to quit! or other key continue. ");
+		c=getchar();
+		if(c==EOF||c=='q'){
+			return 0;
 		}
 	}
 	return 0;
